c43befca3e299bf904a0cd97338692bc7bdf4810
SeokYoung <willy4202@naver.com>
2022-05-09 18:24:59 +0900

    weCLass200 초기세팅 완료

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -6,6 +6,7 @@ const GlobalStyle = createGlobalStyle`
     body{
         padding: 0;
         margin: 0;
+        box-sizing: border-box;
         font-family: 'Noto Sans KR', sans-serif;
     };
     button{

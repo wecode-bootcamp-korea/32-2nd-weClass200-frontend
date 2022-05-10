0a65353ee8b099b8655011a2bc5277eecc250543
Young <willy4202@naver.com>
2022-05-10 13:58:33 +0900

    Modify: 경로 수정

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import Router from "./Router";
 import GlobalStyle from "./styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
-import theme from "./styles/Theme";
+import theme from "./styles/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

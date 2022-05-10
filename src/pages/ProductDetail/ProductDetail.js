e68d6fdf48a467d9dedfe3dd9e4abdb8dbb72194
jaewonyang <didwodnjs7268@gmail.com>
2022-05-10 21:44:56 +0900

    Modify : 세부 레이아웃 수정 완료

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,23 +1,29 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import ProductDetailMain from "./ProductDetailMain/ProductDetailMain";
 import ProductDetailAside from "./ProductDetailAside/ProductDetailAside.js";
-import { DETAIL_API } from "../../config.js";
+import { config } from "../../config.js";
 
 const ProductDetail = () => {
   const [detail, setDetail] = useState([]);
+  const params = useParams();
 
-  useEffect(() => {
-    fetch(`${DETAIL_API.detail}`, {
+  const getData = () => {
+    fetch(`${config.list}/public/${params.id}`, {
       method: "GET",
     })
       .then(res => res.json())
       .then(res => setDetail(res.product));
-  }, []);
+  };
+
+  useEffect(() => {
+    getData();
+  }, [params.id]);
 
   return (
     <Wrapper>
-      <ProductDetailMain detail={detail} setDetail={setDetail} />
+      <ProductDetailMain detail={detail} getData={getData} />
       <ProductDetailAside detail={detail} />
     </Wrapper>
   );
